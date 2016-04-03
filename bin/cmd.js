385c5dd877363f29c103a4477ccf84fdfc26dce7
Evan Schwartz <evan.mark.schwartz@gmail.com>
2016-04-03 14:03:10 -0700

    Add inquierer for interactive CLI

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,28 +3,55 @@
 var paymentLicense = require('../')
 var fs = require('fs')
 var path = require('path')
+var inquirer = require('inquirer')
 
-var fileName = process.argv[2]
-if (!fileName) {
-  throw new Error('payment-license <path_to_file>')
-}
-// TODO use https://github.com/SBoudrias/Inquirer.js/
-// TODO use the async versions of fs functions
-var filePath = path.resolve(process.cwd(), fileName)
-var file = fs.readFileSync(filePath)
-paymentLicense.addToFile(file, {
-  creator_account: 'https://red.ilpdemo.org/ledger/accounts/alice',
-  creator_public_key: 'r/MV0THsvdcUAw7Y8x8ca2/dEc8gXRQNDapQ6xFUG3E=', // secret: 9u9KxA3aAQ+9rMZTuXtJjZMOImQwzhHWVGea9oPyqj2v8xXRMey91xQDDtjzHxxrb90RzyBdFA0NqlDrEVQbcQ==
-  price_per_minute: '0.0001'
-})
-.then(function (fileWithLicense) {
-  fs.writeFileSync(filePath, fileWithLicense)
+var partialFilePath = process.argv[2]
 
-  return paymentLicense.parseLicense(fileWithLicense)
-    .then(function (license) {
-      console.log('Added license: ' + license)
-    })
-})
-.catch(function (err) {
-  console.error('Error: ' + err.message)
+// TODO validate input
+console.log('Welcome to the payment-license creator tool')
+var prompts = [{
+  type: 'input',
+  name: 'fileName',
+  message: 'File(s) to add license to:',
+  default: partialFilePath
+}, {
+  type: 'input',
+  name: 'creator_account',
+  // TODO accept alice@red.ilpdemo.org form
+  message: 'ILP account address to receive payments (e.g. https://red.ilpdemo.org/ledger/accounts/alice):',
+  // TODO get rid of defaults
+  default: 'https://red.ilpdemo.org/ledger/accounts/alice'
+}, {
+  type: 'input',
+  name: 'creator_public_key',
+  message: 'Ed25519 public key to sign receipts with:',
+  default: 'r/MV0THsvdcUAw7Y8x8ca2/dEc8gXRQNDapQ6xFUG3E=' // secret: 9u9KxA3aAQ+9rMZTuXtJjZMOImQwzhHWVGea9oPyqj2v8xXRMey91xQDDtjzHxxrb90RzyBdFA0NqlDrEVQbcQ==
+}, {
+  type: 'input',
+  name: 'price_per_minute',
+  message: 'Price per minute to charge for license:',
+  default: '0.0001'
+}]
+
+inquirer.prompt(prompts, function (params) {
+  // TODO use the async versions of fs functions
+  // TODO handle directories
+  var filePath = path.resolve(process.cwd(), params.fileName)
+  var file = fs.readFileSync(filePath)
+  paymentLicense.addToFile(file, {
+    creator_account: params.creator_account,
+    creator_public_key: params.creator_public_key,
+    price_per_minute: params.price_per_minute
+  })
+  .then(function (fileWithLicense) {
+    fs.writeFileSync(filePath, fileWithLicense)
+
+    return paymentLicense.parseLicense(fileWithLicense)
+      .then(function (license) {
+        console.log('Added license: ' + license)
+      })
+  })
+  .catch(function (err) {
+    console.error('Error: ' + err.message)
+  })
 })
